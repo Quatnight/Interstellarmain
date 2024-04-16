5cf07677299fc0a72d0e2f3c7905dbc6bd741df2
aftermight <167148913+aftermight@users.noreply.github.com>
2024-04-16 14:18:32 +0000

    Updated config.js

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const config = {
-  challenge: false, // Set to true
+  challenge: true, // Set to true
   //if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    username:'password'
+    sam:'owned',
+    will:'willybilly',
+    cj:'rah',
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
