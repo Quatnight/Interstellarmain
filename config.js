b893b6891667e689c0d88fe6d510a0b0e4930a40
aftermight <167148913+aftermight@users.noreply.github.com>
2024-04-16 09:24:03 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const config = {
   if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    aftermight: 'jarhead992!',
-    monkey: 'tickler'
+    aftermight: 'jarhead992!'.
+    monkey: 'tickler',
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
